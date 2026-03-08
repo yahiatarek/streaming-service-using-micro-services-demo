8f2eb2702e46ddada093548c938655fef8e211f5
yahia.tarek <yahia.tarek@vodafone.com>
2026-03-08 23:52:17 +0200

    add 2nd instance to try load balancing

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/Proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/Proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.in28minutes.microservices.currency_conversion_service.Entity.CurrencyExchange;
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyExchange currencyExchange(@PathVariable String from, @PathVariable String to);
---
+++ currency-exchange-service-2/src/main/java/com/in28minutes/microservices/currency_exchange_service/Controller/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.in28minutes.microservices.currency_exchange_service.Controller;
+import com.in28minutes.microservices.currency_exchange_service.Entity.CurrencyExchange;
+import com.in28minutes.microservices.currency_exchange_service.Repository.CurrencyExchangeRepository;
+
+import java.math.BigDecimal;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+    private Environment environment;
+    private CurrencyExchangeRepository repository;
+    
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
+        this.environment = environment;
+        this.repository = repository;
+    }
+    
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange currencyExchange(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to Find Data for " + from + " to " + to);
+        }
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+        return currencyExchange;
+    }
+}
