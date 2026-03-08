fbbdbb86b8b115c40e49e2439f542c53076e23a9
yahia.tarek <yahia.tarek@vodafone.com>
2026-03-08 23:19:37 +0200

    naming server connected

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/Controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/Controller/CurrencyConversionController.java
@@ -3,42 +3,40 @@ package com.in28minutes.microservices.currency_conversion_service.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 
 import com.in28minutes.microservices.currency_conversion_service.Entity.CurrencyConversion;
+import com.in28minutes.microservices.currency_conversion_service.Entity.CurrencyExchange;
+import com.in28minutes.microservices.currency_conversion_service.Proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
+    private final CurrencyExchangeProxy currencyExchangeProxy;
+    
+    public CurrencyConversionController(CurrencyExchangeProxy currencyExchangeProxy) {
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+    
     @GetMapping("/currency-Conversion/quantity/{quantity}/{from}/{to}")
     public CurrencyConversion retrieveConversionValue(
         @PathVariable BigDecimal quantity,
         @PathVariable String from,
         @PathVariable String to
     ) {
-        RestTemplate http = new RestTemplate();
-
-        ResponseEntity<CurrencyConversion> response = 
-            http.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
-            CurrencyConversion.class,
-            from, to
-        );
-
-        CurrencyConversion body = response.getBody();
-        if (body == null) {
-            return null;
+        CurrencyExchange currencyExchange = currencyExchangeProxy.currencyExchange(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to Find Data for " + from + " to " + to);
         }
 
         return new CurrencyConversion(
-            body.getId(),
+            currencyExchange.getId(),
             from,
             to,
             quantity,
-            body.getConversionMultiple(),
-            quantity.multiply(body.getConversionMultiple()),
-            body.getEnvironment()
+            currencyExchange.getConversionMultiple(),
+            quantity.multiply(currencyExchange.getConversionMultiple()),
+            currencyExchange.getEnvironment() + " now from feign"
         );
     }
 }
