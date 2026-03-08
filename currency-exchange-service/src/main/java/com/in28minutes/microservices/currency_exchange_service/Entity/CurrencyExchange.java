d7461e81c7098130256e5cadc52d24a3fc4ef1e0
yahia.tarek <yahia.tarek@vodafone.com>
2026-03-08 07:25:05 +0200

    wip

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/Entity/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/Entity/CurrencyExchange.java
@@ -3,19 +3,30 @@ package com.in28minutes.microservices.currency_exchange_service.Entity;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class CurrencyExchange {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
+    @Column(name = "conversion_multiple")
     private BigDecimal conversionMultiple;
+
     private String environment;
+
+    public CurrencyExchange() {
+    }
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple, String environment) {
         this.id = id;
